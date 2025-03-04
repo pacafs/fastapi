702ecffddedd75f5ca06d8157d3cd76d6f0586cc
Pedro Fernandes da Silva <paca.fs@gmail.com>
2025-03-04 04:13:34 +0000

    Enable JWT token authentication for tasks endpoint

--- app/routers/tasks.py
+++ app/routers/tasks.py
@@ -10,8 +10,8 @@ router = APIRouter()
 # Create a JWT bearer instance
 jwt_bearer = JWTBearer()
 # Create a dependency to check the token
-checkToken = Annotated[str, Depends(lambda: "test_token")] # jwt_bearer
-
+#checkToken = Annotated[str, Depends(lambda: "test_token")] # jwt_bearer
+checkToken = Annotated[str, Depends(jwt_bearer)]
 # GET to /tasks from the prefix
 @router.get("", response_model=List[Task])
 def get_tasks(token: checkToken, session: pgSession):
